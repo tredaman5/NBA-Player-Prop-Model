c9ebd85c4b75889c2b0e64496da0b930cd0cd8af
tredaman5 <smithtre1076@gmail.com>
2026-02-09 14:51:24 -0500

    modifying build_dataset

--- src/props/points/build_dataset.py
+++ src/props/points/build_dataset.py
@@ -19,7 +19,7 @@ import numpy as np
 import pandas as pd
 
 
-REPO_ROOT = Path(__file__).resolve().parents[4]
+REPO_ROOT = Path(__file__).resolve().parents[3]
 DATA_PROCESSED = REPO_ROOT / "data" / "processed"
 
 
